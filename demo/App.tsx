80109b5aafa11f0ff577e3232a41d3cc882230dd
MainShayne233 <shaynetremblay@hotmail.com>
2018-08-21 00:29:32 -0400

    Get basic demo app up

--- demo/App.tsx
+++ demo/App.tsx
@@ -3,10 +3,153 @@ import * as React from 'react';
 import { hot } from 'react-hot-loader';
 import NeatComponent from '../src/NeatComponent';
 
-const App = () => (
+type Dimensions = {
+  height: number;
+  width: number;
+};
+
+type Position = {
+  x: number;
+  y: number;
+};
+
+type AppProps = {};
+type AppState = {
+  parent: App;
+  borderPadding: number;
+  dimensions: Dimensions;
+  position: Position;
+};
+
+const IMAGE_HREF =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Elm_logo.svg/2000px-Elm_logo.svg.png';
+
+const INITIAL_STATE = {
+  borderPadding: 10,
+  dimensions: {
+    height: 100,
+    width: 100,
+  },
+  position: {
+    x: 100,
+    y: 100,
+  },
+};
+
+const StateForm = ({
+  dimensions,
+  position,
+  borderPadding,
+  parent,
+}: AppState) => (
+  <form>
+    <fieldset>
+      <legend>Dimensions</legend>
+      <p>
+        <label htmlFor="height">Height</label>
+        <input
+          onChange={parent.handleHeightChange.bind(parent)}
+          type="number"
+          name="height"
+          value={dimensions.height}
+        />
+        <label htmlFor="width">Width</label>
+        <input
+          onChange={parent.handleWidthChange.bind(parent)}
+          type="number"
+          name="width"
+          value={dimensions.width}
+        />
+      </p>
+      <legend>Position</legend>
+      <p>
+        <label htmlFor="x">X</label>
+        <input
+          onChange={parent.handlePositionXChange.bind(parent)}
+          type="number"
+          name="x"
+          value={position.x}
+        />
+        <label htmlFor="y">Y</label>
+        <input
+          onChange={parent.handlePositionYChange.bind(parent)}
+          type="number"
+          name="y"
+          value={position.y}
+        />
+      </p>
+      <legend>Misc</legend>
+      <p>
+        <label htmlFor="borderPadding">Border Padding</label>
+        <input name="borderPadding" value={borderPadding} />
+      </p>
+    </fieldset>
+  </form>
+);
+
+const AppView = (state: AppState) => (
   <div>
-    <NeatComponent neatMessageProp="Woooo! The demo is working!" />
+    <svg height="500" width="500">
+      <image
+        height={state.dimensions.height}
+        width={state.dimensions.width}
+        preserveAspectRatio="none"
+        href={IMAGE_HREF}
+      />
+    </svg>
+    {StateForm(state)}
   </div>
 );
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: object) {
+    super(props);
+    this.state = { ...INITIAL_STATE, parent: this };
+  }
+
+  handleHeightChange({ target }: React.ChangeEvent<HTMLInputElement>) {
+    const parsedValue = parseInt(target.value);
+    if (!isNaN(parsedValue)) {
+      this.setState({
+        ...this.state,
+        dimensions: { ...this.state.dimensions, height: parsedValue },
+      });
+    }
+  }
+
+  handleWidthChange({ target }: React.ChangeEvent<HTMLInputElement>) {
+    const parsedValue = parseInt(target.value);
+    if (!isNaN(parsedValue)) {
+      this.setState({
+        ...this.state,
+        dimensions: { ...this.state.dimensions, width: parsedValue },
+      });
+    }
+  }
+
+  handlePositionXChange({ target }: React.ChangeEvent<HTMLInputElement>) {
+    const parsedValue = parseInt(target.value);
+    if (!isNaN(parsedValue)) {
+      this.setState({
+        ...this.state,
+        position: { ...this.state.position, x: parsedValue },
+      });
+    }
+  }
+
+  handlePositionYChange({ target }: React.ChangeEvent<HTMLInputElement>) {
+    const parsedValue = parseInt(target.value);
+    if (!isNaN(parsedValue)) {
+      this.setState({
+        ...this.state,
+        position: { ...this.state.position, y: parsedValue },
+      });
+    }
+  }
+
+  render() {
+    return AppView({ ...this.state });
+  }
+}
 
 export default hot(module)(App);
--- src/NeatComponent.tsx
+++ src/NeatComponent.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 
-const NeatComponent: React.SFC<{neatMessageProp: string}> = ({neatMessageProp} : { neatMessageProp: string}) => (
+const NeatComponent: React.SFC<{ neatMessageProp: string }> = ({
+  neatMessageProp,
+}: {
+  neatMessageProp: string,
+}) => (
   <div>
     <p id="message">{neatMessageProp}</p>
   </div>
